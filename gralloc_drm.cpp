maurossi/hardware_drm_gralloc

commit 3c014213a91a1b8c8cc902408adc9fa7cd49277b

WIP
Signed-off-by: Robert Foss <robert.foss@collabora.com>

gralloc_drm.cpp
@@ -32,10 +32,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <tuple>
+#include <unordered_map>
+
+#include <android/gralloc_handle.h>
+
 #include "gralloc_drm.h"
 #include "gralloc_drm_priv.h"
 
 #define unlikely(x) __builtin_expect(!!(x), 0)
+
+static std::unordered_map<buffer_handle_t, struct gralloc_drm_bo_t *> drm_bo_handle_map;
 
 static int32_t gralloc_drm_pid = 0;
 
@@ -186,39 +193,33 @@ int gralloc_drm_get_fd(struct gralloc_drm_t *drm)
 static struct gralloc_drm_bo_t *validate_handle(buffer_handle_t _handle,
 		struct gralloc_drm_t *drm)
 {
-	struct gralloc_drm_handle_t *handle = gralloc_drm_handle(_handle);
+	if (drm_bo_handle_map.count(_handle)) {
+		gralloc_drm_bo_t *bo = drm_bo_handle_map[_handle];
+		return bo;
+	}
 
+	/* check only */
+	if (!drm)
+		return NULL;
+
+	struct gralloc_handle_t *handle = gralloc_handle(_handle);
 	if (!handle)
 		return NULL;
 
-	/* the buffer handle is passed to a new process */
-	ALOGE("data_owner=%d gralloc_pid=%d data=%p\n", handle->data_owner, gralloc_drm_get_pid(), handle->data);
-	if (unlikely(handle->data_owner != gralloc_drm_pid)) {
-		struct gralloc_drm_bo_t *bo;
+	/* create the struct gralloc_drm_bo_t locally */
+	ALOGE("handle: name=%d pfd=%d\n", handle->name, handle->prime_fd);
+	struct gralloc_drm_bo_t *bo;
+	if (handle->name || handle->prime_fd >= 0)
+		bo = drm->drv->alloc(drm->drv, handle);
+	else /* an invalid handle */
+		return NULL;
 
-		/* check only */
-		if (!drm)
-			return NULL;
+	bo->drm = drm;
+	bo->imported = 1;
+	bo->handle = handle;
+	bo->refcount = 1;
 
-		ALOGE("handle: name=%d pfd=%d\n", handle->name,
-			handle->prime_fd);
-		/* create the struct gralloc_drm_bo_t locally */
-		if (handle->name || handle->prime_fd >= 0)
-			bo = drm->drv->alloc(drm->drv, handle);
-		else /* an invalid handle */
-			bo = NULL;
-		if (bo) {
-			bo->drm = drm;
-			bo->imported = 1;
-			bo->handle = handle;
-			bo->refcount = 1;
-		}
-
-		handle->data_owner = gralloc_drm_get_pid();
-		handle->data = bo;
-	}
-
-	return handle->data;
+	return bo;
 }
 
 /*
@@ -256,41 +257,15 @@ int gralloc_drm_handle_unregister(buffer_handle_t handle)
 }
 
 /*
- * Create a buffer handle.
- */
-static struct gralloc_drm_handle_t *create_bo_handle(int width,
-		int height, int format, int usage)
-{
-	struct gralloc_drm_handle_t *handle;
-
-	handle = new gralloc_drm_handle_t;
-	if (!handle)
-		return NULL;
-
-	handle->base.version = sizeof(handle->base);
-	handle->base.numInts = GRALLOC_DRM_HANDLE_NUM_INTS;
-	handle->base.numFds = GRALLOC_DRM_HANDLE_NUM_FDS;
-
-	handle->magic = GRALLOC_DRM_HANDLE_MAGIC;
-	handle->width = width;
-	handle->height = height;
-	handle->format = format;
-	handle->usage = usage;
-	handle->prime_fd = -1;
-
-	return handle;
-}
-
-/*
  * Create a bo.
  */
 struct gralloc_drm_bo_t *gralloc_drm_bo_create(struct gralloc_drm_t *drm,
 		int width, int height, int format, int usage)
 {
 	struct gralloc_drm_bo_t *bo;
-	struct gralloc_drm_handle_t *handle;
+	struct gralloc_handle_t *handle;
 
-	handle = create_bo_handle(width, height, format, usage);
+	handle = gralloc_handle(gralloc_handle_create(width, height, format, usage));
 	if (!handle)
 		return NULL;
 
@@ -305,9 +280,6 @@ struct gralloc_drm_bo_t *gralloc_drm_bo_create(struct gralloc_drm_t *drm,
 	bo->handle = handle;
 	bo->fb_id = 0;
 	bo->refcount = 1;
-
-	handle->data_owner = gralloc_drm_get_pid();
-	handle->data = bo;
 
 	return bo;
 }
@@ -325,11 +297,7 @@ static void gralloc_drm_bo_destroy(struct gralloc_drm_bo_t *bo)
 		return;
 
 	bo->drm->drv->free(bo->drm->drv, bo);
-	if (imported) {
-		handle->data_owner = 0;
-		handle->data = 0;
-	}
-	else {
+	if (!imported) {
 		delete handle;
 	}
 }
@@ -367,8 +335,8 @@ buffer_handle_t gralloc_drm_bo_get_handle(struct gralloc_drm_bo_t *bo, int *stri
 void gralloc_drm_resolve_format(buffer_handle_t _handle,
 	uint32_t *pitches, uint32_t *offsets, uint32_t *handles)
 {
-	struct gralloc_drm_handle_t *handle = gralloc_drm_handle(_handle);
-	struct gralloc_drm_bo_t *bo = handle->data;
+	struct gralloc_drm_handle_t *handle = gralloc_handle(_handle);
+	struct gralloc_drm_bo_t *bo = drm_bo_handle_map[_handle];
 	struct gralloc_drm_t *drm = bo->drm;
 
 	/* if handle exists and driver implements resolve_format */
@@ -381,7 +349,7 @@ void gralloc_drm_resolve_format(buffer_handle_t _handle,
  * Lock a bo.  XXX thread-safety?
  */
 int gralloc_drm_bo_lock(struct gralloc_drm_bo_t *bo,
-		int usage, int x, int y, int w, int h,
+		uint32_t usage, int x, int y, int w, int h,
 		void **addr)
 {
 	if ((bo->handle->usage & usage) != usage) {

Android.mk
@@ -59,7 +59,7 @@ LOCAL_MODULE := libgralloc_drm
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_CFLAGS := -std=c11 -Wno-unused-parameter
+LOCAL_CFLAGS := -Wno-unused-parameter
 
 LOCAL_SRC_FILES := \
 	gralloc_drm.cpp
@@ -127,6 +127,9 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	gralloc.cpp
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+	$(LOCAL_PATH)
 
 LOCAL_C_INCLUDES := \
 	external/libdrm \

gralloc_drm_handle.h
@@ -1,92 +1,0 @@
-/*
- * Copyright (C) 2010-2011 Chia-I Wu <olvaffe@gmail.com>
- * Copyright (C) 2010-2011 LunarG Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-#ifndef _GRALLOC_DRM_HANDLE_H_
-#define _GRALLOC_DRM_HANDLE_H_
-
-#include <cutils/native_handle.h>
-#include "gralloc_drm_formats.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-struct gralloc_drm_bo_t;
-
-struct gralloc_drm_handle_t {
-	native_handle_t base;
-
-	/* file descriptors */
-	int prime_fd;
-
-	/* integers */
-	int magic;
-
-	int width;
-	int height;
-	int format;
-	int usage;
-
-	int name;   /* the name of the bo */
-	int stride; /* the stride in bytes */
-
-	struct gralloc_drm_bo_t *data; /* pointer to struct gralloc_drm_bo_t */
-
-	// FIXME: the attributes below should be out-of-line
-	uint64_t unknown __attribute__((aligned(8)));
-	int data_owner; /* owner of data (for validation) */
-};
-#define GRALLOC_DRM_HANDLE_MAGIC 0x12345678
-#define GRALLOC_DRM_HANDLE_NUM_FDS 1
-#define GRALLOC_DRM_HANDLE_NUM_INTS (						\
-	((sizeof(struct gralloc_drm_handle_t) - sizeof(native_handle_t))/sizeof(int))	\
-	 - GRALLOC_DRM_HANDLE_NUM_FDS)
-
-#define GRALLOC_DRM_HANDLE_MAGIC 0x12345678
-#define GRALLOC_DRM_HANDLE_NUM_FDS 0
-#define GRALLOC_DRM_HANDLE_NUM_INTS (						\
-	((sizeof(struct gralloc_drm_handle_t) - sizeof(native_handle_t))/sizeof(int))	\
-	 - GRALLOC_DRM_HANDLE_NUM_FDS)
-
-static inline struct gralloc_drm_handle_t *gralloc_drm_handle(buffer_handle_t _handle)
-{
-	struct gralloc_drm_handle_t *handle =
-		(struct gralloc_drm_handle_t *) _handle;
-
-	if (handle && (handle->base.version != sizeof(handle->base) ||
-	               handle->base.numInts != GRALLOC_DRM_HANDLE_NUM_INTS ||
-	               handle->base.numFds != GRALLOC_DRM_HANDLE_NUM_FDS ||
-	               handle->magic != GRALLOC_DRM_HANDLE_MAGIC)) {
-		ALOGE("invalid handle: version=%d, numInts=%d, numFds=%d, magic=%x",
-			handle->base.version, handle->base.numInts,
-			handle->base.numFds, handle->magic);
-		handle = NULL;
-	}
-
-	return handle;
-}
-
-#ifdef __cplusplus
-}
-#endif
-#endif /* _GRALLOC_DRM_HANDLE_H_ */

gralloc_drm_priv.h
@@ -28,7 +28,7 @@
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 
-#include "gralloc_drm_handle.h"
+#include <android/gralloc_handle.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -53,7 +53,7 @@ struct gralloc_drm_drv_t {
 
 	/* allocate or import a bo */
 	struct gralloc_drm_bo_t *(*alloc)(struct gralloc_drm_drv_t *drv,
-			                  struct gralloc_drm_handle_t *handle);
+			                  struct gralloc_handle_t *bo);
 
 	/* free a bo */
 	void (*free)(struct gralloc_drm_drv_t *drv,
@@ -76,10 +76,11 @@ struct gralloc_drm_drv_t {
 
 struct gralloc_drm_bo_t {
 	struct gralloc_drm_t *drm;
-	struct gralloc_drm_handle_t *handle;
+	struct gralloc_handle_t *handle;
 
 	int imported;  /* the handle is from a remote proces when true */
 	int fb_handle; /* the GEM handle of the bo */
+	int gem_name;  /* the GEM name of the bo */
 	int fb_id;     /* the fb id */
 
 	int lock_count;

commit b295a5e511d35b88c98ee0ae86ec3f55cfab4b20

remove unused gralloc_drm_get_pid()
Not used anymore

Fixes: 3c014213 ("WIP")

gralloc_drm.cpp
@@ -44,19 +44,6 @@
 
 static std::unordered_map<buffer_handle_t, struct gralloc_drm_bo_t *> drm_bo_handle_map;
 
-static int32_t gralloc_drm_pid = 0;
-
-/*
- * Return the pid of the process.
- */
-static int gralloc_drm_get_pid(void)
-{
-	if (unlikely(!gralloc_drm_pid))
-		android_atomic_write((int32_t) getpid(), &gralloc_drm_pid);
-
-	return gralloc_drm_pid;
-}
-
 /*
  * Create the driver for a DRM fd.
  */