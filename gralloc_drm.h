maurossi/hardware_drm_gralloc

commit 642a775519b80e28ae3a286aa29d89e9890acbab

drm_gralloc: Use render node
Change-Id: I9bbee113e884d0c512a855e85fa88db4e3789a1e
Signed-off-by: Sean Paul <seanpaul@chromium.org>
Signed-off-by: Tomasz Figa <tfiga@google.com>

gralloc_drm.h
@@ -40,21 +40,13 @@ struct gralloc_drm_t;
 struct gralloc_drm_bo_t;
 
 enum {
-	GRALLOC_MODULE_PERFORM_GET_DRM_FD                = 0x40000002,
-	GRALLOC_MODULE_PERFORM_GET_DRM_MAGIC             = 0x80000003,
-	GRALLOC_MODULE_PERFORM_AUTH_DRM_MAGIC            = 0x80000004,
-	GRALLOC_MODULE_PERFORM_ENTER_VT                  = 0x80000005,
-	GRALLOC_MODULE_PERFORM_LEAVE_VT                  = 0x80000006,
+	GRALLOC_MODULE_PERFORM_GET_DRM_FD                = 0x80000002,
 };
 
 struct gralloc_drm_t *gralloc_drm_create(void);
 void gralloc_drm_destroy(struct gralloc_drm_t *drm);
 
 int gralloc_drm_get_fd(struct gralloc_drm_t *drm);
-int gralloc_drm_get_magic(struct gralloc_drm_t *drm, int32_t *magic);
-int gralloc_drm_auth_magic(struct gralloc_drm_t *drm, int32_t magic);
-int gralloc_drm_set_master(struct gralloc_drm_t *drm);
-void gralloc_drm_drop_master(struct gralloc_drm_t *drm);
 
 static inline int gralloc_drm_get_bpp(int format)
 {

Android.mk
@@ -62,6 +62,7 @@ LOCAL_SHARED_LIBRARIES := \
 	liblog \
 	libcutils \
 	libhardware_legacy \
+	libutils
 
 ifneq ($(filter $(freedreno_drivers), $(DRM_GPU_DRIVERS)),)
 LOCAL_SRC_FILES += gralloc_drm_freedreno.c
@@ -112,12 +113,13 @@ LOCAL_SRC_FILES := \
 
 LOCAL_C_INCLUDES := \
 	external/libdrm \
-	external/libdrm/include/drm \
+	external/libdrm/include/drm
 
 LOCAL_SHARED_LIBRARIES := \
 	libgralloc_drm \
 	libdrm \
 	liblog \
+	libutils
 
 # for glFlush/glFinish
 LOCAL_SHARED_LIBRARIES += \

gralloc.cpp
@@ -71,30 +71,6 @@ static int drm_mod_perform(const struct gralloc_module_t *mod, int op, ...)
 			err = 0;
 		}
 		break;
-	/* should we remove this and next ops, and make it transparent? */
-	case GRALLOC_MODULE_PERFORM_GET_DRM_MAGIC:
-		{
-			int32_t *magic = va_arg(args, int32_t *);
-			err = gralloc_drm_get_magic(dmod->drm, magic);
-		}
-		break;
-	case GRALLOC_MODULE_PERFORM_AUTH_DRM_MAGIC:
-		{
-			int32_t magic = va_arg(args, int32_t);
-			err = gralloc_drm_auth_magic(dmod->drm, magic);
-		}
-		break;
-	case GRALLOC_MODULE_PERFORM_ENTER_VT:
-		{
-			err = gralloc_drm_set_master(dmod->drm);
-		}
-		break;
-	case GRALLOC_MODULE_PERFORM_LEAVE_VT:
-		{
-			gralloc_drm_drop_master(dmod->drm);
-			err = 0;
-		}
-		break;
 	default:
 		err = -EINVAL;
 		break;
@@ -217,8 +193,10 @@ unlock:
 
 static int drm_mod_close_gpu0(struct hw_device_t *dev)
 {
+	struct drm_module_t *dmod = (struct drm_module_t *)dev->module;
 	struct alloc_device_t *alloc = (struct alloc_device_t *) dev;
 
+	gralloc_drm_destroy(dmod->drm);
 	delete alloc;
 
 	return 0;

gralloc_drm.cpp
@@ -25,6 +25,7 @@
 
 #include <cutils/log.h>
 #include <cutils/atomic.h>
+#include <cutils/properties.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -121,6 +122,7 @@ static const char *fbdrv_map[][2] = {
  */
 struct gralloc_drm_t *gralloc_drm_create(void)
 {
+	char path[PROPERTY_VALUE_MAX];
 	struct gralloc_drm_t *drm;
 	FILE *fb;
 	unsigned card, ret = 0;
@@ -139,19 +141,11 @@ struct gralloc_drm_t *gralloc_drm_create(void)
 	if (!drm)
 		return NULL;
 
-	drm->fd = -1;
-	for (card = 0; card < sizeof(fbdrv_map) / sizeof(const char *) / 2; ++card) {
-		if (!strcmp(buf, fbdrv_map[card][0])) {
-			drm->fd = drmOpen(fbdrv_map[card][1], NULL);
-			ALOGD("drmOpen %s: %d", fbdrv_map[card][1], drm->fd);
-			break;
-		}
-	}
-
+	property_get("gralloc.drm.device", path, "/dev/dri/renderD128");
+	drm->fd = open(path, O_RDWR);
 	if (drm->fd < 0) {
-		ALOGE("failed to open driver for %s", buf);
-	} else {
-		drm->drv = init_drv_from_fd(drm->fd);
+		ALOGE("failed to open %s", path);
+		return NULL;
 	}
 
 	if (!drm->drv) {
@@ -183,42 +177,6 @@ int gralloc_drm_get_fd(struct gralloc_drm_t *drm)
 }
 
 /*
- * Get the magic for authentication.
- */
-int gralloc_drm_get_magic(struct gralloc_drm_t *drm, int32_t *magic)
-{
-	return drmGetMagic(drm->fd, (drm_magic_t *) magic);
-}
-
-/*
- * Authenticate a magic.
- */
-int gralloc_drm_auth_magic(struct gralloc_drm_t *drm, int32_t magic)
-{
-	return drmAuthMagic(drm->fd, (drm_magic_t) magic);
-}
-
-/*
- * Set as the master of a DRM device.
- */
-int gralloc_drm_set_master(struct gralloc_drm_t *drm)
-{
-	ALOGD("set master");
-	drmSetMaster(drm->fd);
-	drm->first_post = 1;
-
-	return 0;
-}
-
-/*
- * Drop from the master of a DRM device.
- */
-void gralloc_drm_drop_master(struct gralloc_drm_t *drm)
-{
-	drmDropMaster(drm->fd);
-}
-
-/*
  * Validate a buffer handle and return the associated bo.
  */
 static struct gralloc_drm_bo_t *validate_handle(buffer_handle_t _handle,
@@ -230,15 +188,18 @@ static struct gralloc_drm_bo_t *validate_handle(buffer_handle_t _handle,
 		return NULL;
 
 	/* the buffer handle is passed to a new process */
-	if (unlikely(handle->data_owner != gralloc_drm_get_pid())) {
+	ALOGE("data_owner=%d gralloc_pid=%d data=%p\n", handle->data_owner, gralloc_drm_get_pid(), handle->data);
+	if (unlikely(handle->data_owner != gralloc_drm_pid)) {
 		struct gralloc_drm_bo_t *bo;
 
 		/* check only */
 		if (!drm)
 			return NULL;
 
+		ALOGE("handle: name=%d pfd=%d\n", handle->name,
+			handle->prime_fd);
 		/* create the struct gralloc_drm_bo_t locally */
-		if (handle->name)
+		if (handle->name || handle->prime_fd >= 0)
 			bo = drm->drv->alloc(drm->drv, handle);
 		else /* an invalid handle */
 			bo = NULL;
@@ -311,6 +272,7 @@ static struct gralloc_drm_handle_t *create_bo_handle(int width,
 	handle->height = height;
 	handle->format = format;
 	handle->usage = usage;
+	handle->prime_fd = -1;
 
 	return handle;
 }
@@ -393,18 +355,6 @@ buffer_handle_t gralloc_drm_bo_get_handle(struct gralloc_drm_bo_t *bo, int *stri
 	if (stride)
 		*stride = bo->handle->stride;
 	return &bo->handle->base;
-}
-
-int gralloc_drm_get_gem_handle(buffer_handle_t _handle)
-{
-	struct gralloc_drm_handle_t *handle = gralloc_drm_handle(_handle);
-	return (handle) ? handle->name : 0;
-}
-
-int gralloc_drm_get_prime_fd(buffer_handle_t _handle)
-{
-	struct gralloc_drm_handle_t *handle = gralloc_drm_handle(_handle);
-	return (handle) ? handle->prime_fd : -1;
 }
 
 /*

gralloc_drm_handle.h
@@ -39,6 +39,7 @@ struct gralloc_drm_handle_t {
 	/* file descriptors */
 	int prime_fd;
 
+	/* integers */
 	int magic;
 
 	int width;
@@ -49,12 +50,17 @@ struct gralloc_drm_handle_t {
 	int name;   /* the name of the bo */
 	int stride; /* the stride in bytes */
 
+	struct gralloc_drm_bo_t *data; /* pointer to struct gralloc_drm_bo_t */
+
+	// FIXME: the attributes below should be out-of-line
+	uint64_t unknown __attribute__((aligned(8)));
 	int data_owner; /* owner of data (for validation) */
-	union {
-		struct gralloc_drm_bo_t *data; /* pointer to struct gralloc_drm_bo_t */
-		int64_t __padding;
-	} __attribute__((aligned(8)));
 };
+#define GRALLOC_DRM_HANDLE_MAGIC 0x12345678
+#define GRALLOC_DRM_HANDLE_NUM_FDS 1
+#define GRALLOC_DRM_HANDLE_NUM_INTS (						\
+	((sizeof(struct gralloc_drm_handle_t) - sizeof(native_handle_t))/sizeof(int))	\
+	 - GRALLOC_DRM_HANDLE_NUM_FDS)
 
 #define GRALLOC_DRM_HANDLE_MAGIC 0x12345678
 #define GRALLOC_DRM_HANDLE_NUM_FDS 0
@@ -70,8 +76,12 @@ static inline struct gralloc_drm_handle_t *gralloc_drm_handle(buffer_handle_t _h
 	if (handle && (handle->base.version != sizeof(handle->base) ||
 	               handle->base.numInts != GRALLOC_DRM_HANDLE_NUM_INTS ||
 	               handle->base.numFds != GRALLOC_DRM_HANDLE_NUM_FDS ||
-	               handle->magic != GRALLOC_DRM_HANDLE_MAGIC))
+	               handle->magic != GRALLOC_DRM_HANDLE_MAGIC)) {
+		ALOGE("invalid handle: version=%d, numInts=%d, numFds=%d, magic=%x",
+			handle->base.version, handle->base.numInts,
+			handle->base.numFds, handle->magic);
 		handle = NULL;
+	}
 
 	return handle;
 }

commit 6bcaa5b585b197e8329b7ae4d638b8ed166bce07

add back GRALLOC_MODULE_PERFORM_{ENTER,LEAVE}_VT enums
libsurfaceflinger requires GRALLOC_MODULE_PERFORM_{ENTER,LEAVE}_VT
defined in gralloc_drm.h, fixes the following building errors:

frameworks/native/services/surfaceflinger/DisplayDevice.cpp:273:29:
error: use of undeclared identifier 'GRALLOC_MODULE_PERFORM_LEAVE_VT'
            gr->perform(gr, GRALLOC_MODULE_PERFORM_LEAVE_VT);
                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
...
frameworks/native/services/surfaceflinger/DisplayDevice.cpp:278:29:
error: use of undeclared identifier 'GRALLOC_MODULE_PERFORM_ENTER_VT'
            gr->perform(gr, GRALLOC_MODULE_PERFORM_ENTER_VT);
                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
2 errors generated.

Fixes: 642a7755 ("drm_gralloc: Use render node") for android-x86 build

gralloc_drm.h
@@ -41,6 +41,8 @@ struct gralloc_drm_bo_t;
 
 enum {
 	GRALLOC_MODULE_PERFORM_GET_DRM_FD                = 0x80000002,
+	GRALLOC_MODULE_PERFORM_ENTER_VT                  = 0x80000005,
+	GRALLOC_MODULE_PERFORM_LEAVE_VT                  = 0x80000006,
 };
 
 struct gralloc_drm_t *gralloc_drm_create(void);